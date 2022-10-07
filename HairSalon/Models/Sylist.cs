183b905a8e7cdea598a704f4d3fa65a176c06b6c
Skylan Lew <skylanslew@gmail.com>
2022-10-07 13:40:20 -0700

    update Stylist model

--- HairSalon/Models/Sylist.cs
+++ HairSalon/Models/Sylist.cs
@@ -12,5 +12,6 @@ namespace HairSalon.Models
     public int StylistId { get; set; }
     public string StylistName { get; set; }
     public string StylistShift { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
   }
 }
